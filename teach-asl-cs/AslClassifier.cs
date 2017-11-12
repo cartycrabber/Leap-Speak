f07d628afa9a8fcd121f5e096477729ec008c7d6
Will Carty <cartycrabber@gmail.com>
2017-11-12 03:27:00 -0500

    create windows forms app

---
+++ teach-asl-cs/AslClassifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Accord.MachineLearning;
+using System.IO;
+
+namespace TeachAslCsharp
+{
+
+    class AslClassifier
+    {
+        AslClassifier()
+        {
+
+        }
+
+        void LoadTrainingData(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+        }
+
+        void Train()
+        {
+
+        }
+
+        int Predict()
+        {
+            return -1;
+        }
+    }
+}
