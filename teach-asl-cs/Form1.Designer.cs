127f1370cac7922722880c0f2a9c974cfa962377
Ian Boll <lordofthemaracas@gmail.com>
2017-11-12 05:14:54 -0500

    made a simple slideshow

--- teach-asl-cs/Form1.Designer.cs
+++ teach-asl-cs/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.nextSymbol = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previousButton
+            // 
+            this.previousButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.previousButton.Location = new System.Drawing.Point(12, 606);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(386, 146);
+            this.previousButton.TabIndex = 1;
+            this.previousButton.Text = "Previous Symbol";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1455, 588);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // nextSymbol
+            // 
+            this.nextSymbol.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextSymbol.Location = new System.Drawing.Point(1115, 606);
+            this.nextSymbol.Name = "nextSymbol";
+            this.nextSymbol.Size = new System.Drawing.Size(352, 146);
+            this.nextSymbol.TabIndex = 4;
+            this.nextSymbol.Text = "Next Symbol";
+            this.nextSymbol.UseVisualStyleBackColor = true;
+            this.nextSymbol.Click += new System.EventHandler(this.nextSymbol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1479, 764);
+            this.Controls.Add(this.nextSymbol);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.previousButton);
+            this.Name = "Form1";
+            this.Text = "Teach ASL";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button nextSymbol;
     }
 }
 
--- teach-asl-cs/Form1.cs
+++ teach-asl-cs/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,45 @@ namespace teach_asl_cs
 {
     public partial class Form1 : Form
     {
+        string[] symbolImages = new string[0];
+        int symbolIndex = 3;
+
         public Form1()
         {
             InitializeComponent();
+            SetupSlideShow();
+        }
+
+        private void SetupSlideShow()
+        {
+            symbolImages = Directory.GetFiles("assets");
+            Array.Sort(symbolImages);
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            if (symbolIndex < symbolImages.Length && symbolIndex >= 0)
+            {
+                pictureBox.ImageLocation = symbolImages[symbolIndex];
+            }
+        }
+
+        private void nextSymbol_Click(object sender, EventArgs e)
+        {
+            ++symbolIndex;
+            symbolIndex = symbolIndex % symbolImages.Length;
+            UpdateImage();
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            --symbolIndex;
+            if (symbolIndex < 0)
+            {
+                symbolIndex = symbolImages.Length - 1;
+            }
+            UpdateImage();
         }
     }
 }
